8195288ffad8e8ece8894d41bdd6527d8c56e8ec
William Pollet <william.pollet@kisskissbankbank.com>
2018-10-30 10:40:33 +0100

    Only one message per file

--- lib/pronto/pronto_simplecov/runner.rb
+++ lib/pronto/pronto_simplecov/runner.rb
@@ -12,8 +12,9 @@ module Pronto
         file_coverage = coverage[patch.new_file_full_path.to_s]
         return unless file_coverage
         patch.added_lines
-          .select { |line| file_coverage.line(line.new_lineno).missed? unless file_coverage.line(line.new_lineno).nil? }
+             .select { |line| file_coverage.line(line.new_lineno).missed? unless file_coverage.line(line.new_lineno).nil? }
              .map { |line| message(line) }
+             [0...1]
       end
 
       def message(line)
